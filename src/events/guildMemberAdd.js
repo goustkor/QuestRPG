7e9de5dd6a95be1939570d8582d997910f540e15
unknown <goustkor@outlook.com>
2019-05-14 11:12:42 -0300

    Welcome message public/private In-Progress

---
+++ src/events/guildMemberAdd.js
@@ -0,0 +1,44 @@
+﻿module.exports = async (client, member) => {
+	
+	const embedPublic = {
+	  "color": 8311585,
+	  "timestamp": new Date(),
+	  "footer": {
+		"icon_url": client.user.avatarURL,
+		"text": "#welcome_message_date"
+	  },
+	  "author": {
+		"name": member.user.username,
+		"icon_url": member.user.avatarURL
+	  },
+	  "fields": [
+		{
+		  "name": "#welcome_message_title",
+		  "value": "#welcome_message_public"
+		}
+	  ]
+	};
+
+	const embedPrivate = {
+	  "color": 8311585,
+	  "timestamp": new Date(),
+	  "footer": {
+		"icon_url": client.user.avatarURL,
+		"text": "#welcome_message_date"
+	  },
+	  "author": {
+		"name": member.user.username,
+		"icon_url": member.user.avatarURL
+	  },
+	  "fields": [
+		{
+		  "name": "#Welcome_message_private_field_1_title",
+		  "value": "#welcome_message_private_field_1_content"
+		}
+	  ]
+	};
+	//member.guild.channels.find("name","general").send({embedPublic});
+	//client.send({embedPrivate});
+	client.users.get(member.user.id).send({embed: embedPrivate});
+	//console.log(member);
+}
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const client = new Discord.Client();
 const config = require("./config.json");
 client.config = config;
 
-fs.readdir("./events/", (err, files) => {
+fs.readdir("./src/events/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
     const event = require(`./events/${file}`);
@@ -17,7 +17,7 @@ fs.readdir("./events/", (err, files) => {
 
 client.commands = new Enmap();
 
-fs.readdir("./commands/", (err, files) => {
+fs.readdir("./src/commands/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
     if (!file.endsWith(".js")) return;
